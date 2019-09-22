7a9831e1f80bc298cd658ee79b5ed5ed81097286
Afeawo <ea446115@ohio.edu>
2019-09-22 15:23:24 -0400

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EA446115_MIS4200.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EA446115_MIS4200.Startup))]
+namespace EA446115_MIS4200
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
